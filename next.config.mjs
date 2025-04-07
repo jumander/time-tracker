b98e89e234480e85ca921593a81c5a213c36f1d3
Johannes Umander <johannes.umander@ume.se>
2025-04-07 20:51:26 +0200

    Add gh-pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/time-tracker' : '',
+    assetPrefix: isProd ? '/time-tracker' : '',
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 'use client';
 import { useState } from 'react';
 
-export default function MyElement()
+export default function MyButton()
 {
+  const [count, setCount] = useState(0);
+
+  function handleClick()
+  {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <button>React</button>
+      <button onClick={handleClick}>React</button>
+      <p>Button pressed {count} times</p>
     </div>
   )
 }
