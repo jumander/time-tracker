e83c24835225f1046ea92787b23319a64d1ca996
Johannes Umander <johannes.umander@ume.se>
2025-04-07 20:55:55 +0200

    Fix website is not interactive

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     basePath: isProd ? '/time-tracker' : '',
-    assetPrefix: isProd ? '/time-tracker' : '',
+    assetPrefix: isProd ? '/time-tracker/' : '',
 };
 
 export default nextConfig;
